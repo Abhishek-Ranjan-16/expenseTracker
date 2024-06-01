491ed7046aa718e91673ef4e259c0af2ad71775c
AssassinOFsilchar <abhiran14082002@gmail.com>
2024-06-01 21:12:22 +0530

    fixed db app

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth,GoogleAuthProvider} from 'firebase/auth'
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,14 +15,13 @@ const firebaseConfig = {
   storageBucket: "expense-tracker-6f514.appspot.com",
   messagingSenderId: "666589135408",
   appId: "1:666589135408:web:f6e427eca5b981ada5e3f5",
-  measurementId: "G-KZ13ZYWXRT"
+  measurementId: "G-KZ13ZYWXRT",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-
-export const auth=getAuth(app)
-export const provider=new GoogleAuthProvider()
-export const db=getFirestore()
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
